158f7b7bc95a3e78212cf43c0c138a18f271fd9b
evgenij <samitin.e@gmail.com>
2022-06-10 18:41:54 +1100

    NotesDao

---
+++ app/src/main/java/ru/samitin/notesapp/Dao/NotesDao.kt
@@ -0,0 +1,19 @@
+package ru.samitin.notesapp.Dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import ru.samitin.notesapp.Model.Notes
+
+@Dao
+interface NotesDao {
+    @Query("SELECT * FROM Notes ")
+    fun getNotes():LiveData<List<Notes>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertNotes(notes : Notes)
+
+    @Query("DELETE FROM Notes WHERE id=:id")
+    fun deleteNotes(id : Int)
+    @Update
+    fun updateNotes(notes: Notes)
+}
